972f0293548b197bd8a3871afe035af01c033140
Manisha-batesar <manubatesar@gmail.com>
2025-04-09 15:37:29 +0530

    add PreparationTime component and enhance Recipes layout with styling

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -3,7 +3,10 @@ import './Recipes.css'
 
 const Recipes = () => {
   return (
-    <div>Recipes</div>
+    <div className='recipeContent'>
+      <h1>Simple Omelette Recipe</h1>
+      <p>An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats. </p>
+    </div>
   )
 }
 
