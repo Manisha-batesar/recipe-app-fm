0521a29236f4b832b7e91e5b152657268c2b49a9
Manisha-batesar <manubatesar@gmail.com>
2025-04-09 16:00:16 +0530

    add PreparationTime component and fix title in index.html

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './styles/global.css'
 import Header from './components/Header/Header';
 import Recipes from './components/Recipes/Recipes';
+import PreparationTime from './components/PreparationTime/PreparationTime';
 
 const App = () => {
   return (
     <>
       <Header />
       <Recipes />
+      <PreparationTime />
     </>
   )
 }
---
+++ src/components/PreparationTime/PreparationTime.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './PreparationTime.css'
+
+const PreparationTime = () => {
+  return (
+    <div className='preparationtime'>
+
+      <h2>Preparation time</h2>
+      <ul>
+        <li><span>Total:</span>Approximately 10 minutes</li>
+        <li><span>Preparation:</span>5 minutes </li>
+        <li><span>Cooking: </span>5 minutes </li>
+      </ul>
+    </div>
+  )
+}
+
+export default PreparationTime
